82faeb92b253d262ff7a59a3df0cd60f64902c63
costeer <costeer@protonmail.com>
2025-04-07 19:29:33 +0200

    init

--- src/consts.ts
+++ src/consts.ts
@@ -1,20 +1,20 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+  NAME: "Costeer.dev",
+  EMAIL: "costeer@protonmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
+  NUM_WORKS_ON_HOMEPAGE: 0,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
+  TITLE: "~/home",
   DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
 };
 
 export const BLOG: Metadata = {
-  TITLE: "Blog",
+  TITLE: "~/notes",
   DESCRIPTION: "A collection of articles on topics I am passionate about.",
 };
 
@@ -25,20 +25,17 @@ export const WORK: Metadata = {
 
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+  DESCRIPTION:
+    "A collection of my projects, with links to repositories and demos.",
 };
 
 export const SOCIALS: Socials = [
-  { 
-    NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
-  },
-  { 
-    NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
-  },
-  { 
-    NAME: "linkedin",
+  {
+    NAME: "links",
     HREF: "https://www.linkedin.com/in/markhorn-dev",
-  }
+  },
+  {
+    NAME: "github",
+    HREF: "https://github.com/markhorn-dev",
+  },
 ];
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,14 +3,22 @@ import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        subTitle: "#9C9E9F",
+        subText: "#9EA1A1",
+        bg: "#9EA1A1",
+        bgl: "#06080A",
+        bgll: "#303030",
+        nothingRed: "#D71921",
+      },
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-        serif: ["Lora", ...defaultTheme.fontFamily.serif],
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+        serif: ["Roboto", ...defaultTheme.fontFamily.serif],
+        ntype82: ["NType82", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
       },
     },
   },
